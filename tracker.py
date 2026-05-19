93443e202078fcdb8423af6b93235ce028e2a71f
24149bhs <24149@burnside.school.nz>
2026-05-20 10:24:57 +1200

    test sql

--- tracker.py
+++ tracker.py
@@ -1,1 +1,22 @@
-print('hi')
+#docstring Christchurch Tigers-players database application
+#imports
+import sqlite3
+
+#contant and variables
+DATABASE = "team"
+#functions
+def print_all_playername():
+    '''print all the playername nicely'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "select * from players;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"Name                          Date           Attendance     Score")
+    for player in results:
+        print(f"{player[1]:<30}{player[2]:<15}{player[3]:<15}{player[4]:<6}")
+    #loop finish here
+    db.close()
+#main code
+print_all_playername()

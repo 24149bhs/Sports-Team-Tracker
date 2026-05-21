025ba49b1a22277485478542c59109914e37cb72
24149bhs <24149@burnside.school.nz>
2026-05-22 09:24:51 +1200

    finish

--- tracker.py
+++ tracker.py
@@ -5,7 +5,7 @@ import sqlite3
 #contant and variables
 DATABASE = "team"
 #functions
-def print_all_playername():
+def print_all_player():
     '''print all the playername nicely'''
     db = sqlite3.connect(DATABASE)
     cursor = db.cursor()
@@ -18,5 +18,69 @@ def print_all_playername():
         print(f"{player[1]:<30}{player[2]:<15}{player[3]:<15}{player[4]:<6}")
     #loop finish here
     db.close()
+    
+def print_all_player_by_name():
+    '''print all the players stored by name'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "select * from players order by playername desc;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"Name                          Date           Attendance     Score")
+    for player in results:
+        print(f"{player[1]:<30}{player[2]:<15}{player[3]:<15}{player[4]:<6}")
+    #loop finish here
+    db.close()
+
+def print_all_player_by_attendance():
+    '''print all the players stored by attendance'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "select * from players order by attendance desc;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"Name                          Date           Attendance     Score")
+    for player in results:
+        print(f"{player[1]:<30}{player[2]:<15}{player[3]:<15}{player[4]:<6}")
+    #loop finish here
+    db.close()
+
+def print_all_player_by_score():
+    '''print all the players stored by point scored'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "select * from players or2der by pointscored desc;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print(f"Name                          Date           Attendance     Score")
+    for player in results:
+        print(f"{player[1]:<30}{player[2]:<15}{player[3]:<15}{player[4]:<6}")
+    #loop finish here
+    db.close()
 #main code
-print_all_playername()
+while True:
+    user_input = input(
+'''
+What would you like to do.
+1. Print all players
+2. Print all players by names
+3. Print all players by attendance
+4. Print all players by point scored
+5. Exit
+'''
+    )
+    if user_input == "1":
+        print_all_player()
+    elif user_input == "2":
+        print_all_player_by_name()
+    elif user_input == "3":
+        print_all_player_by_attendance()
+    elif user_input == "4":
+        print_all_player_by_score()
+    elif user_input == "5":
+        break
+    else:
+        print("That was not an opition\n")
